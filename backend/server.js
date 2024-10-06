3c36fa9614f415b6c3e584ed416ca3c41e28ba23
Muhammed Sinan Mp <sinanmhd5817@gmail.com>
2024-10-06 14:03:19 +0530

    d

--- backend/server.js
+++ backend/server.js
@@ -17,11 +17,12 @@ connectDB()
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: "http://localhost:5173",
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Ensure OPTIONS is included
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
+  origin: 'http://localhost:5173', // Allow only localhost:5173
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Ensure OPTIONS is included for pre-flight requests
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allow necessary headers
+  credentials: true, // Allow credentials (cookies, auth headers)
 };
+
 
 
 app.get("/",(req,res)=>{
