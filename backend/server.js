e820a7ba1e173eccaf3856bbf4571ad2fb175c9f
Muhammed Sinan Mp <sinanmhd5817@gmail.com>
2024-10-07 22:09:02 +0530

    hello

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ connectDB()
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://192.168.137.1:5173', 'http://192.168.31.121:5173'], // Ensure mobile IP is included
+  origin: ['http://localhost:5173', "https://callcenter.codeandclick.in"], // Ensure mobile IP is included
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
