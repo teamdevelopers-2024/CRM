6c1e6c47262829d99275d1def94a6e1c28c24064
Muhammed Sinan Mp <sinanmhd5817@gmail.com>
2024-10-08 01:08:21 +0530

    d

--- backend/server.js
+++ backend/server.js
@@ -17,11 +17,19 @@ connectDB()
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: ['http://localhost:5173', "https://callcenter.codeandclick.in"], // Ensure mobile IP is included
+  origin: (origin, callback) => {
+    const allowedOrigins = ['http://localhost:5173', 'https://crm.codeandclick.in'];
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 };
+
 
 app.use(cors(corsOptions));
 
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3001/api",
+    baseURL: "https://crmapi.codeandclick.in/api",
     headers: {
         "Content-Type": "application/json",
     },
