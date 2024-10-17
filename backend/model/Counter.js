1308ac4a82b0e6438d6ae6d3dd06e22ac077be22
Muhammed Sinan Mp <sinanmhd5817@gmail.com>
2024-10-17 18:48:45 +0530

    fixed type

--- backend/model/Counter.js
+++ backend/model/Counter.js
@@ -1,13 +1,14 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const counter = new mongoose.Schema({
-    count : {
-      try:Number ,
-      default : -1
+// Define the counter schema
+const counterSchema = new mongoose.Schema({
+    count: {
+        type: Number, // Corrected typo
+        default: -1   // Start with -1 if no lead is assigned yet
     }
-  })
+});
 
+// Create a Counter model from the schema
+const Counter = mongoose.model("Counter", counterSchema);
 
-const Counter = mongoose.model("Counter",counter)
-
-export default Counter
+export default Counter;
