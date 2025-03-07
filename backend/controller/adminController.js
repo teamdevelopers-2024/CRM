1c901d2027811c8095ca0914278a6656f04ae592
Mpdanish <danishmaliyekkal786@gmail.com>
2025-03-07 16:43:00 +0530

    asljf

--- backend/controller/adminController.js
+++ backend/controller/adminController.js
@@ -95,7 +95,7 @@ async function adminLogin(req, res) {
                 message: "admin logged in successfully",
                 role: "admin"
             })
-        } else if (username == "supercode@123" &&  password == "1234") {
+        } else if (username == process.env.SUPERADMIN_USERNAME &&  password == "1234") {
             
             return res.status(200).json({
                 error: false,
