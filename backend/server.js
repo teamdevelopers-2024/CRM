7e8e8d90e2cbd64ae367e1f1a49b8d58b003501d
Mpdanish <danishmaliyekkal786@gmail.com>
2025-03-07 15:20:13 +0530

    asljf

--- backend/server.js
+++ backend/server.js
@@ -3,43 +3,66 @@ import cors from 'cors'; // Importing cors
 import 'dotenv/config'; // Loads environment variables
 import router from "./router/adminRouter.js";
 import connectDB from "./database/connection.js";
-import { findAndDeleteLeads } from "./database/getLead.js";
+import {findAndDeleteLeads } from "./database/getLead.js";
 import empRouter from "./router/employeeRouter.js";
 
 const app = express();
 
-// Configure port
 const PORT = process.env.PORT || 5000;
 
-// Connect to DB
-connectDB();
+app.use(express.json());   
+  
+connectDB()     
+// findAndDeleteLeads()
+
+app.use(express.urlencoded({ extended: true }));
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    const allowedOrigins = ["http://localhost:5173", "https://crm.codeandclick.in","http://192.168.137.1:5173"];
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+};
+
+app.use((req, res, next) => {
+  console.log('Incoming request:', req.method, req.url, 'from origin:', req.get('Origin'));
+  next();
+});
+
+app.get("/confirmation",(req,res)=>{
+  const response = req.query.response
+  res.send(response,'this is the response')
+})
 
 
-
-
-app.use(cors()); // Enable CORS
-app.use(express.json()); // Parse incoming JSON requests
-
-
-// Error-handling middleware (should be last)
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
 
-// Default route
+
+
+app.use(cors(corsOptions));
+
 app.get("/", (req, res) => {
   res.json({
     message: "everything is fine"
   });
 });
 
-// Use your routers
+
 app.use('/api', router);
 app.use('/api', empRouter);
 
 // Start the server
-app.listen(PORT, "0.0.0.0", (err) => {
+app.listen(PORT,"0.0.0.0", (err) => {
   if (err) {
     console.error("Error starting the server:", err);
   } else {
